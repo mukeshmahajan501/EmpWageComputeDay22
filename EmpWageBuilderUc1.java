39aaa583495e5afae34c968dfef222b8fabc1f9b
mukesh <mukeshmahajan501@gmail.com>
2020-07-11 05:15:40 +0530

    checked employee is present or not

---
+++ EmpWageBuilderUc1.java
@@ -0,0 +1,18 @@
+class EmpWageBuilderUc1
+{
+
+public static void main(String args[])
+{
+double empCheck=Math.floor(Math.random()*10%2);
+	if (empCheck==1)
+	{
+		System.out.println("employee is present");
+		System.out.println(no);
+	}
+	else
+	{
+		System.out.println("employee is absent");
+	}
+
+}
+}
